71f3c963d0c242b64f45a9334bcd9721fced08dd
VLLindqvist <victor.lindqvist@outlook.com>
2022-02-27 23:45:58 -0700

    Fixed layout issue in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "media",
   theme: {
     colors: {
